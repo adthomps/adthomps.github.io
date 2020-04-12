6f11c6f62e881d61c330b79bfedaac7fc8ea980d
Adam <athompson2008@gmail.com>
2020-04-12 16:22:32 -0700

    update js

--- stylescripts/basic.js
+++ stylescripts/basic.js
@@ -20,16 +20,4 @@ $('.ui.sidebar')
     ;
 
 })
-
-
-// Lightbox for photos using Semantic UI
-
-$('.PhotoCard').click(function() {
-        var image = $(this).children('img').attr('src');
-        $('body').append('<div class="ui basic modal"><div class="content"><img src="'+image+'" width="100%" /></div></div>');        
-        $('.ui.basic.modal')
-            .remove()
-            .modal('show');
-})
-
 ;
