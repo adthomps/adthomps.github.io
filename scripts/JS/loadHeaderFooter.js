a608dd418d0bfa319d6bac18ceb5fac3740a0996
Adam <athompson2008@gmail.com>
2024-07-13 13:43:30 -0700

    update5

--- scripts/JS/loadHeaderFooter.js
+++ scripts/JS/loadHeaderFooter.js
@@ -3,15 +3,20 @@ document.addEventListener('DOMContentLoaded', function() {
 });
 
 function loadHeaderFooter() {
-  fetch('templates/header.html')
+  // Determine the relative path to the templates directory
+  const path = window.location.pathname.split('/').slice(0, -1).join('/') + '/templates/';
+  
+  fetch(path + 'header.html')
     .then(response => response.text())
     .then(data => {
       document.querySelector('header').innerHTML = data;
-    });
+    })
+    .catch(error => console.error('Error loading header:', error));
 
-  fetch('templates/footer.html')
+  fetch(path + 'footer.html')
     .then(response => response.text())
     .then(data => {
       document.querySelector('footer').innerHTML = data;
-    });
+    })
+    .catch(error => console.error('Error loading footer:', error));
 }
