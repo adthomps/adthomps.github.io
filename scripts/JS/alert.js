813c3e010601f94f0d4b195d9dde0a59ff871719
Adam <athompson2008@gmail.com>
2024-07-26 18:35:39 -0700

    Update alert.js

--- scripts/JS/alert.js
+++ scripts/JS/alert.js
@@ -1,19 +1,20 @@
 const alertPlaceholder = document.getElementById('liveAlertPlaceholder')
 const appendAlert = (message, type) => {
-  const wrapper = document.createElement('div')
-  wrapper.innerHTML = [
-    `<div class="alert alert-${type} alert-dismissible" role="alert">`,
-    `   <div>${message}</div>`,
-    '   <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>',
-    '</div>'
-  ].join('')
+    const wrapper = document.createElement('div')
+    wrapper.innerHTML = [
+        `<div class="alert alert-${type} alert-dismissible" role="alert">`,
+        `<div><i class="bi bi-exclamation-triangle-fill"></i>  ${message}</div>`,
+        `<br /><button type="button" class="btn btn-warning" data-bs-toggle="modal" data-bs-target="#exampleModal">Launch Alert Details</button>`,
+        '<button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>',
+        '</div>'
+    ].join('')
 
-  alertPlaceholder.append(wrapper)
+    alertPlaceholder.append(wrapper)
 }
 
 const alertTrigger = document.getElementById('liveAlertBtn')
 if (alertTrigger) {
-  alertTrigger.addEventListener('click', () => {
-    appendAlert('Nice, you triggered this alert message!', 'success')
-  })
+    alertTrigger.addEventListener('click', () => {
+        appendAlert('Excellent, you have triggered the alert test. Click the button below to get more details on the alert.', 'danger')
+    })
 }
