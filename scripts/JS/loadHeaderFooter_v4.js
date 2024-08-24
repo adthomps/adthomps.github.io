06a70bde2a5b5e4deb5db7469f3051f23eca9c4d
Adam <athompson2008@gmail.com>
2024-08-24 10:18:47 -0700

    Update loadHeaderFooter_v4.js

--- scripts/JS/loadHeaderFooter_v4.js
+++ scripts/JS/loadHeaderFooter_v4.js
@@ -4,7 +4,7 @@ document.addEventListener("DOMContentLoaded", function () {
   const sidebarPath = getRelativePath("templates/sidebar.html");
   const footerPath = getRelativePath("templates/footer.html");
 
-  loadHTML(headerPath, "header");
+  loadHTML(headerPath, "header2");
   loadHTML(sidebarPath, "sidebar");
   loadHTML(footerPath, "footer");
 });
