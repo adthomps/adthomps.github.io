dcdcbd8c76e1ba63d1873931f69529525a2da784
Adam <athompson2008@gmail.com>
2024-11-28 14:36:58 -0800

    Create config.js

---
+++ anet/accept/js/config.js
@@ -0,0 +1,7 @@
+// config.js
+const ENV_CONFIG = {
+    apiLoginIDProd: "62W3Efur",
+    clientKeyProd: "4abK6fG25BtW9QF3LZUN84FsmvQh47bpdm8FJ2yA28r9p5WPB34H8N6n7whjUsBu",
+    apiLoginIDSandbox: "62W3Efur",
+    clientKeySandbox: "4abK6fG25BtW9QF3LZUN84FsmvQh47bpdm8FJ2yA28r9p5WPB34H8N6n7whjUsBu"
+};
