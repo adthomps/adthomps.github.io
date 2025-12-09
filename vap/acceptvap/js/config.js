60bb3f0b61c2498f392b0b16388a1d8c43fc2505
Adam <athompson2008@gmail.com>
2025-12-08 20:42:26 -0800

    vaptest setup

---
+++ vap/acceptvap/js/config.js
@@ -0,0 +1,7 @@
+// config.js
+const ENV_CONFIG = {
+    apiLoginIDProd: "APIID",
+    clientKeyProd: "APIKey",
+    apiLoginIDSandbox: "8Y3q4YkmD2Fg",
+    clientKeySandbox: "7e687BabZdAxfsJzqN7mJLwhxrUAH2fBVNKvYD6Ty4Rv67UKY5eJb8G4sREWpD7G"
+};
